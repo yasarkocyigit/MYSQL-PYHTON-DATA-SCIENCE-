2deb691c4a34db10bb48fa7ea8d7904fbf90ac83
Yasar Kocyigit <58749386+yasarkocyigit@users.noreply.github.com>
2020-04-13 00:06:56 -0500

    Add files via upload

---
+++ CREATING DATABASE copy.py
@@ -0,0 +1,28 @@
+"""
+i will upload everything about mysql as part once you learn previous part you will be ok for next one!
+
+i uploaded how to connect mysql to python as previous project.
+
+"""
+
+import mysql.connector
+
+
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user ="root",
+    passwd = "gs163264"
+)
+
+# on this lesson we will learn how create a database
+
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE IF NOT EXISTS test1")
+# when you check mysql application you will see there is a "matrixdb" databes
+
+# IF YOU DONT USE " IF NOT EXISTS " once you run code you will get syntax error next time
+# its because once you execute code you will have that named database and you cant have 2 same named database
+# and also you wont get syntax error if you use " IF NOT EXISTS "
